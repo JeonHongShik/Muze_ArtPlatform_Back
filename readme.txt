eaa3947de503f56093997eda328c9080a2b896db
HOTARU <dusckd4948@naver.com>
2023-07-25 21:16:41 +0900

    upload
    upload

--- readme.txt
+++
@@ -1,5 +0,0 @@
-version 1.0
-	ㄴ 회원정보(accounts) CRUD 완료
-
-version 1.1
-	ㄴ 전공자게시물(consumers) CRUD 완료
--- version1.0/accounts/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import User
-from rest_framework import serializers
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("__all__")
-        # fields = ('id','Name','Email','Profile','Created','Updated')
