f23c058a26392ab70abfb504380839dae9252029
HOTARU <dusckd4948@naver.com>
2023-07-25 21:10:15 +0900

    First Murge
    posts, consumers, accounts Murge

--- 1.0/posts/urls.py
+++
@@ -1,9 +0,0 @@
-# posts/urls.py
-
-from django.urls import path
-from .views import PerformancePostList, PerformancePostDetail
-
-urlpatterns = [
-    path('', PerformancePostList.as_view(), name='post_list'),
-    path('<int:pk>/', PerformancePostDetail.as_view(), name='post_detail')
-]
---
+++ version1.0/consumers/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import ConsumUser
+from .serializers import ConsumerSerializer
+from rest_framework.viewsets import ModelViewSet
+
+# Create your views here.
+class ConsumList(ModelViewSet):
+    queryset = ConsumUser.objects.all()
+    serializer_class = ConsumerSerializer
+    
+class ConsumDetail(ModelViewSet):
+    queryset = ConsumUser.objects.all()
+    serializer_class = ConsumerSerializer
+    
