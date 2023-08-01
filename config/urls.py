8a55a800366e6d40f818170c1707f60c20adeeae
Yeoun changhyun <dusckd4948@naver.com>
2023-08-01 17:15:12 +0900

    123

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
     path("consumer/", include("consumer.urls")),
+    path("post/", include("post.urls")),
 ]
--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostDetail,PostList
+from .views import PostDetail, PostList
 
 urlpatterns = [
-    path('', PostList.as_view(), name='post-list'),
+    path("", PostList.as_view(), name="post-list"),
+    path("<int:pk>/", PostDetail.as_view(), name="post-detail"),
 ]
