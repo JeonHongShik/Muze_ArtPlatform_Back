39575ff9d1d557faf9f8c53c6d7e4464692d0087
HOTARU <dusckd4948@naver.com>
2023-08-06 01:20:10 +0900

    migrate

--- post/models.py
+++ post/models.py
@@ -3,17 +3,22 @@
 
 from django.db import models
 from datetime import datetime
-from account.models import UserModel
+from config import settings
 
 
 class PerformancePost(models.Model):
     author = models.ForeignKey(
-        UserModel, on_delete=models.CASCADE, related_name="Post_author"
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="Post_author"
     )  # id값 들어감
 
     agency = models.CharField(max_length=50, null=False, default="기관명")  # 주최기관
     title = models.CharField(max_length=200)  # 포스트 제목
     call = models.CharField(max_length=50)  # 연락수단
+    pay = models.CharField(
+        max_length=50,
+        null=True,
+        default="0",
+    )  #
     info = models.TextField()  # 공연정보
     type = models.CharField(max_length=50)  # 공연종류
     deadline = models.CharField(max_length=50)  # 모집기한
